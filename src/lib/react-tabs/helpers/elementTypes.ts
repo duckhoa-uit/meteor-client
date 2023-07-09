b16f90445491812eb00beb3dd08b1063e75b72e3
DeeNKay <duckhoa.dev@gmail.com>
2023-07-09 14:56:19 +0700

    chore: remove react-tabs

--- src/lib/react-tabs/helpers/elementTypes.ts
+++
@@ -1,7 +0,0 @@
-function makeTypeChecker(tabsRole: string): (element: any) => boolean {
-  return (element) => !!element.type && element.type.tabsRole === tabsRole
-}
-
-export const isTab = makeTypeChecker("Tab")
-export const isTabList = makeTypeChecker("TabList")
-export const isTabPanel = makeTypeChecker("TabPanel")
